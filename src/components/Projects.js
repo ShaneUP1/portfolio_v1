a0f1c2aebe6b5d7a0df33d1967ddea6737250252
Shane Upchurch <shaneupchurch@gmail.com>
2021-01-11 15:00:34 -0600

    fix project image links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,29 +20,8 @@ export default class Projects extends Component {
                     </Fade>
 
                     <Fade duration={1000} delay={700} >
-                        <Tilt 
-                        options={{
-                            reverse: false,
-                            max: 10,
-                            perspective: 500,
-                            scale: 1,
-                            speed: 100,
-                            transition: true,
-                            axis: null,
-                            reset: true,
-                            easing: 'cubic-bezier(.03,.98,.52,.99)',
-                          }}>
+                        
                     <div className="row">
-                        <a href='https://sformichella.github.io/t4-xoxo/'>
-                        <img className="project-img" src={tictactoe_project} alt='tic-tac-toe project' style={{ boxShadow: "5px 5px 3px gray"}} />
-                        </a>
-                    </div>
-                    </Tilt>
-
-                    <div className="project-info row">
-                        Here's an app we built to show users the different eco-transportation choices within walking distance of their current location. Using a Node Express back end and a React front end, incoporating multiple RESTful API calls to gather data, we were able to display all the options around the user on a Google-map-react component, and then further using basic Javascript to allow users to sign-up and login to get access to their saved locations.<br></br><br></br>
-                        Specific to the Portland area, though it could easily be scaled up to other large cities, we hope it comes in handy!
-                    </div>
                     <Tilt 
                         options={{
                             reverse: false,
@@ -55,12 +34,37 @@ export default class Projects extends Component {
                             reset: true,
                             easing: 'cubic-bezier(.03,.98,.52,.99)',
                           }}>
+                        <a href='https://sformichella.github.io/t4-xoxo/'>
+                        <img className="project-img" src={tictactoe_project} alt='tic-tac-toe project' style={{ boxShadow: "5px 5px 3px gray"}} />
+                        </a>
+                        </Tilt>
+                    </div>
+                    
+
+                    <div className="project-info row">
+                        Here's an app we built to show users the different eco-transportation choices within walking distance of their current location. Using a Node Express back end and a React front end, incoporating multiple RESTful API calls to gather data, we were able to display all the options around the user on a Google-map-react component, and then further using basic Javascript to allow users to sign-up and login to get access to their saved locations.<br></br><br></br>
+                        Specific to the Portland area, though it could easily be scaled up to other large cities, we hope it comes in handy!
+                    </div>
+                    
                     <div className="row">
+                    <Tilt 
+                        options={{
+                            reverse: false,
+                            max: 10,
+                            perspective: 500,
+                            scale: 1,
+                            speed: 100,
+                            transition: true,
+                            axis: null,
+                            reset: true,
+                            easing: 'cubic-bezier(.03,.98,.52,.99)',
+                          }}>
                         <a href='https://git-there-api.netlify.app/'>
                         <img className="project-img" src={gitThereAPI_project} alt='gitThere API project' style={{ boxShadow: "5px 5px 3px gray"}}/>
                         </a>
+                        </Tilt>
                     </div>
-                    </Tilt>
+                    
                     </Fade>
                 </section>
                 
