79c2aa2f1619aeb4295def5f3448423e61f02e81
Shane Upchurch <shaneupchurch@gmail.com>
2021-01-23 19:55:52 -0600

    changes images into public folder and removed imports

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import Title from './Title.js';
 import '../styles/Tech.css';
 import { Fade } from 'react-awesome-reveal';
-import css from '../images/techStack_logos/css-3.svg';
-import html from '../images/techStack_logos/html-5.svg';
-import js from '../images/techStack_logos/javascript.svg';
-import node from '../images/techStack_logos/nodejs-icon.svg';
-import sql from '../images/techStack_logos/postgresql.svg';
-import react from '../images/techStack_logos/react.svg';
 
 export default class Tech extends Component {
   render() {
@@ -16,12 +10,12 @@ export default class Tech extends Component {
         <Title title="TECH STACK" />
         <Fade direction="up" duration={1000} delay={500}>
         <div className="tech-display">
-          <img className="svg-logo"src={html} alt="HTML Logo" />
-          <img className="svg-logo"src={js} alt="Javascript Logo" />
-          <img className="svg-logo" src={css} alt="CSS Logo" />
-          <img className="svg-logo"src={node} alt="Node Logo" />
-          <img className="svg-logo"src={sql} alt="SQL Logo" />
-          <img className="svg-logo"src={react} alt="React Logo" />
+          <img className="svg-logo"src='html-5.svg' alt="HTML Logo" />
+          <img className="svg-logo"src='javascript.svg' alt="Javascript Logo" />
+          <img className="svg-logo" src='css-3.svg' alt="CSS Logo" />
+          <img className="svg-logo"src='nodejs-icon.svg' alt="Node Logo" />
+          <img className="svg-logo"src='postgresql.svg' alt="SQL Logo" />
+          <img className="svg-logo"src='react.svg' alt="React Logo" />
         </div>
         </Fade>
         
